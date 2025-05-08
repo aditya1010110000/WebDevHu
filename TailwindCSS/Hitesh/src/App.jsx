5c8ab3872665301486191ffd13d6720cc3355bc9
Aditya_Singh <adityacode10@gmail.com>
2025-05-08 14:58:18 +0530

    Card component Completed

---
+++ TailwindCSS/Hitesh/src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+
+  {/* 
+  Tailwind Properties
+  1. horizontal padding : px-value
+  2. vertical padding   : py-value
+  3. same with margin mx and my
+  4. max-w-value min-h-value
+  5. font color         : text-color
+  6. hover:bg-white-600
+  7. font size : text-xl, font-bold, font-semibold
+  8. letter spacing : tracking-wide
+  9. line height: leading-snug
+  10. subtle focus inticators: ring-  ion fucking know what the fuck this does
+  11. smooth transitions: transition
+  12. animations: scale and opacity
+  13. Chaning the css at various screen size: md:    lg:   
+  14. h-screen: extends the div to the viewport
+  
+
+  📚 Summary of Most Used Tailwind Utilities:
+Category	Key Classes
+Layout	flex, grid, container, justify-*, items-*
+Spacing	p-*, m-*, gap-*, space-x-*
+Sizing	w-*, h-*, max-w-*, min-h-*
+Color	bg-*, text-*, border-*, hover:*
+Typography	text-*, font-*, tracking-*, leading-*
+Positioning	      relative, absolute, z-*, top-*, left-*
+Borders & Radius	border, rounded, shadow, ring-*
+Effects	transition, duration-*, scale-*, opacity-*
+Responsive	sm:, md:, lg:, xl: prefixes
+State Classes	hover:*, focus:*, disabled:*, group-hover:*
+  
+
+
+    // Code for a card
+
+    
+    <div className="md:max-w-lg max-w-sm bg-pink-900 p-4 rounded-2xl shadow-xl">
+      <div className=" bg-purple-950 rounded-xl p-2 border-purple-400 border-8 shadow-purple-600">
+        <div className='justify-center items-center text-center font-semibold text-2xl mb-2'>
+          A Beautiful Palace
+        </div>
+        <div className='md:flex group overflow-hidden rounded-sm'>
+          <img className="md:w-60 rounded-sm md:mr-4 mb-2 transition-transform group-hover:scale-110 duration-200" src="https://media.gettyimages.com/id/185313478/photo/cmyk-color-guide-and-lorem-ipsum-text.jpg?s=612x612&w=0&k=20&c=llnNEei8KmQBbZkXJYEOl2wdHMs3d9vly7gp9oN4jJk=" alt="Nothing" />
+          <div className="">
+            <p>Lorem ipsum dolor sit amet cons ecte tur adipis ici ng elit. Quo, sunt? Lorem ipsum dolor sit amet cons ect etur adipi sic ing elit. Quo, sunt?</p>
+          </div>
+        </div>
+        <div className='bg-purple-400 hover:bg-purple-500 text-white text-center font-bold py-2  rounded focus:outline-none focus:shadow-outline mt-4'>
+          <button type="button">Click me</button>
+        </div>
+
+      </div>
+    </div>
+  
+  */}
+
+
+ 
+
+
+
+  return (
+
+  )
+
+}
+
+export default App
